0122259e630c9de42b5f0ee0906abeb99a66e4b1
woshiluoshao <2394775223@qq.com>
2020-11-09 15:04:06 +0800

    demo start

---
+++ src/main/java/jp/PmanagerApplication.java
@@ -0,0 +1,13 @@
+package jp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmanagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jp/PmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package jp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
